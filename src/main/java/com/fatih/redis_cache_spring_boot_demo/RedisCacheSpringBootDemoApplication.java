96ecc929e1ad8f036db87c6cedcb93c9ad6bf8d0
fatihhozkurt <fatihhhozkurt@gmail.com>
2025-03-07 21:29:59 +0300

    initial commit

---
+++ src/main/java/com/fatih/redis_cache_spring_boot_demo/RedisCacheSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fatih.redis_cache_spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCacheSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatih/redis_cache_spring_boot_demo/RedisCacheSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.redis_cache_spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
